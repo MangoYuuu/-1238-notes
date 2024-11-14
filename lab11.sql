401bb9b0a331a18f0f4e2c3d6479674495a1826e
MangoYuuu <yuqing.lin@georgebrown.ca>
2024-11-14 10:49:12 -0500

    Update lab11.sql
    All my queries that worked.
    
    Modified based on what I tried in class.

--- lab11.sql
+++ lab11.sql
@@ -1,32 +1,45 @@
-SELECT course_id,course_name
-FROM  courses
-WHERE lab_time LIKE 'Tue%'
-ORDER BY course_id;
+--All my queries that run successfully：
 
+--Step 4: Exercise Question 1
+SELECT title,due_date 
+FROM assignments
+WHERE course_id = 'COMP1234';
+
+--Step 4: Exercise Question 2
+SELECT min(due_date) 
+FROM assignments;
+
+--Step 4: Exercise Question 3
+SELECT max(due_date) 
+FROM assignments;
+
+--Step 4: Exercise Question 4
+SELECT title,course_id
+FROM  assignments
+WHERE due_date = '2024-10-08'
+
+--Step 4: Exercise Question 5
+SELECT title,due_date
+FROM  assignments
+WHERE due_date LIKE '2024-10%'
+
+  
+--Optional task 1:
 SELECT *
 FROM  assignments
 WHERE status = 'Not Started'
 ORDER BY due_date;
 
-
--- The following is the sql that has been provided：
-SELECT * FROM courses;
-
-SELECT * FROM assignments
-LIMIT 10; 
-
-SELECT count(*) FROM courses;
-
-SELECT min(due_date) FROM assignments;
-
-SELECT *
+--Optional task 2:
+SELECT course_id,course_name
 FROM  courses
-WHERE course_name LIKE 'Intro%';
+WHERE lab_time LIKE 'Tue%'
+ORDER BY course_id;
 
-SELECT *
-FROM  assignments
-WHERE status != 'Completed'
-ORDER BY due_date;
+
+
+
+
 
 -- Use this query as a reference for the next step
 SELECT course_id, title, status, due_date
